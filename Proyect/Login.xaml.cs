53bd5337c6eb3a2ccf93fe4660e836833a2cae75
Danny <Danny@DESKTOP-I6446O2>
2021-11-22 21:18:18 -0500

    Add project files.

---
+++ Proyect/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proyect
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcon_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
